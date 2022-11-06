1002c7c3f46181b36a7cf96b5f7999a0e85c4910
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-11-06 16:03:52 +0000

    QOL patch

--- server/main.lua
+++ server/main.lua
@@ -6,7 +6,7 @@ AddEventHandler("qb-coffeeshop:bill:player", function(playerId, amount)
         local biller = QBCore.Functions.GetPlayer(source)
         local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
         local amount = tonumber(amount)
-        if biller.PlayerData.job.name == '420coffee' then
+        if biller.PlayerData.job.name == Config.JobName then
             if billed ~= nil then
                 if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                     if amount and amount > 0 then
